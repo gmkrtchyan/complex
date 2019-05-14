a222b586e9d038fad1508a9e4a0d07b3b8006d0a
Gurgen Mkrtchyan <GMkrtchyan@carsondellosa.com>
2019-05-14 19:37:19 +0400

    initial commit

---
+++ worker/index.js
@@ -0,0 +1,18 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisCLient = redis.createClient({
+    host: keys.redistHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const sub = redisCLient.duplicate();
+
+function fib(index) {
+    if (index < 2) return 1;
+    return fib(index - 1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+    redisCLient.hset('values', message, fib(parseInt(message)));
+});
